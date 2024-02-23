f7dcd02cddb09f5cdba62fc38adb99883be86ed4
Corey Feder <corey.feder@gmail.com>
2024-02-23 01:01:36 -0500

    import not working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./styles.css";
+// This isn't working...
+
+// import("./styles.css");
+
+// import sheet from './styles.css' assert { type: 'css' };
+// document.adoptedStyleSheets = [sheet];
+// shadowRoot.adoptedStyleSheets = [sheet];
+
+// const cssModule = import('./styles.css', { assert: { type: 'css' } } );
+// document.adoptedStyleSheets = [cssModule.default];
+
+// const cssModule = await import('./styles.css', { assert: { type: 'css' } });    
+// document.adoptedStyleSheets = [cssModule.default];
+
+// Hm. Stumped for now.
+
+const mainEl = document.querySelector("main")
+mainEl.style.backgroundColor = 'var(--main-bg);'
+let newNode = document.createElement("h1");
+newNode.textContent = "DOM Manipulation";
+mainEl.appendChild(newNode);
+mainEl.classList.add("flex-ctr")
