5ce4b2ea1e047689d75d5915beed7c8f2c919bb8
Corey Feder <corey.feder@gmail.com>
2024-02-23 03:33:02 -0500

    start work on part two, the graded lab

--- src/index.js
+++ src/index.js
@@ -1,50 +1,83 @@
-// import "./styles.css";
-// This isn't working...
-
-// import("./styles.css");
-
-// import sheet from './styles.css' assert { type: 'css' };
-// document.adoptedStyleSheets = [sheet];
-// shadowRoot.adoptedStyleSheets = [sheet];
-
-// const cssModule = import('./styles.css', { assert: { type: 'css' } } );
-// document.adoptedStyleSheets = [cssModule.default];
-
-// const cssModule = await import('./styles.css', { assert: { type: 'css' } });    
-// document.adoptedStyleSheets = [cssModule.default];
-
-// Hm. Stumped for now.
-
-
 // Menu data structure
 var menuLinks = [
-    { text: "about", href: "/about" },
-    { text: "catalog", href: "/catalog" },
-    { text: "orders", href: "/orders" },
-    { text: "account", href: "/account" },
-  ];
-  
-//   import "./styles.css";
-  
-  // Part 1
-  const mainEl = document.querySelector("main");
-  mainEl.style.backgroundColor = "var(--main-bg)";
-  let newNode = document.createElement("h1");
-  newNode.textContent = "DOM Manipulation";
-  mainEl.appendChild(newNode);
-  mainEl.classList.add("flex-ctr");
-  
-  //Part 2
-  let topMenuEl = document.getElementById("top-menu");
-  topMenuEl.style.height = "100%";
-  topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
-  topMenuEl.classList.add("flex-around");
-  
-  // Part 3
-  for (let link of menuLinks) {
-    let newLink = document.createElement("a");
-    newLink.setAttribute("href", link.href);
-    newLink.textContent = link.text;
-    topMenuEl.appendChild(newLink);
-  }
-  
+  { text: "about", href: "/about" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
+];
+
+import "./styles.css";
+
+// Part 1
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+let newNode = document.createElement("h1");
+newNode.textContent = "DOM Manipulation";
+mainEl.appendChild(newNode);
+mainEl.classList.add("flex-ctr");
+
+//Part 2
+let topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+// Part 3
+for (let link of menuLinks) {
+  let newLink = document.createElement("a");
+  newLink.setAttribute("href", link.href);
+  newLink.textContent = link.text;
+  topMenuEl.appendChild(newLink);
+}
+
+//Part 4
+let subMenuEl = document.getElementById("sub-menu");
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.classList.add("flex-around");
+subMenuEl.style.position = "absolute";
+subMenuEl.style.top = "0";
+
+// Part 5
+let topMenuLinks = document.querySelectorAll("#top-menu a");
+// apPARently, I shouldn't use "anonymous functions" as "callback functions"
+// because the handlers can't be removed "safely" or maybe "at all"
+// so they will "create memory leaks"
+// and the apps will "slow to a crawl" or "crash"
+// especially with "long-running" web apps
+// (and in this age of "tab proliferation", every web app should "expect" to be "long-running")
+// and the "users" will be "angry"
+// and then I'll be "unemployed" "again"
+// and be forced to "learn" to "use" "quotation marks"
+// "pro"per"ly"
+// ...
+// Actually, that article was from 2013. The current documentation indicates it's NBD.
+const handlerTopMentClick = (e) => {
+  e.preventDefault();
+  JSON.stringify(e);
+};
+
+topMenuEl.addEventListener("click", handlerTopMentClick);
