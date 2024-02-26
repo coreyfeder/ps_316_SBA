c630221fe2200e34bdf66080ec32b5686f60770f
Corey Feder <corey.feder@gmail.com>
2024-02-26 12:45:21 -0500

    much update, but still on 2-5

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 // import "./styles.css";
-// ???
-// Well, THIS doesn't work.
-// Best I can figure is this is some misplaced remnant of Next.js or SASS or something.
-// It shouldn't be here without explanation. Or we shouldn't be told we can develop
-// locally and not worry about that hella jank Sandbox.
+/* 
+Well, THAT line doesn't work locally.
+Best I can figure is this is some misplaced remnant of Next.js or SASS or something. Probably used by accident, or maybe someone thought it necessary for the exercise, or maybe it's a trap by CodeSandbox to keep people on their site.
+
+Maybe NBD if you're sure people will stay and work in CodeSandbox, if CodeSandbox were working more reliably; but I'd argue it shouldn't be in a template or boilerplate of an exercise. We can still be put through the DOM manipulation paces even if the page is properly linked to a stylesheet.
+
+Only worth noting because this has been an *enormous* headache trying to set up to work locally. I've spent *hours* trying to work around this "correctly," without feeling like I'm undermining the exercise goals. It's insane. I kept thinking we'd hash it out at office hours, but stuff came up and they were cancelled. Hey, real life happens.
+
+Anyway, for the record: CodeSandbox was causing much _bigger_ headaches. I can sort of deal with weird import routes, but malfunctioning Service Worker registration and straight-up nondeterministic rendering are just too much.
+
+So..._technically_ staying in bounds because I'm not changing the HTML, just the DOM. It feels slimy, but I can't get any work done if I can't actually load the page.
+ */
 let cssLink = document.createElement('link')
 cssLink.setAttribute('rel', 'stylesheet');
 cssLink.setAttribute('href', 'src/styles.css');
 document.getElementsByTagName('head')[0].appendChild(cssLink);
-// <link rel="stylesheet" href="src/styles.css">
 
-
-
-// Turns out, there are a bunch of output tools that are better than console.log(). And I'm livid about it.
-console.count("executing src/index.js"); // This should only ever be 1!!
+/* 
+Holy crap. Turns out, there are a bunch of output tools besides `console.log()`. Logging at different levels, grouping, built-in counters, and ... OFFS WHY DIDN'T ANYONE TELL US ABOUT `debugger`??
+ */
 
 console.groupCollapsed("Initialization");
 
 // Menu data structure
-var menuLinks = [
+//   (Let's consider this the source data. Don't rearrange or rename anything.)
+const menuLinks = [
   { text: "about", href: "/about" },
   {
     text: "catalog",
@@ -47,50 +53,40 @@ var menuLinks = [
     ],
   },
 ];
-console.log("menuLinks", menuLinks);
+// console.log("menuLinks", menuLinks);
 
 // Part 1-1
-console.log("Part 1-1");
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
 let newNode = document.createElement("h1");
 newNode.textContent = "DOM Manipulation";
 mainEl.appendChild(newNode);
 mainEl.classList.add("flex-ctr");
-console.dir(mainEl);
 
 //Part 1-2
-console.log("Part 1-2");
 let topMenuEl = document.getElementById("top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 topMenuEl.classList.add("flex-around");
-console.dir(topMenuEl); // topMenuEl
 
 // Part 1-3
-console.log("Part 1-3");
 for (let link of menuLinks) {
   let newLink = document.createElement("a");
   newLink.setAttribute("href", link.href);
   newLink.textContent = link.text;
   topMenuEl.appendChild(newLink);
 }
-console.dir(topMenuEl);
 
 //Part 2-3
-console.log("Part 2-3");
 let subMenuEl = document.getElementById("sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.classList.add("flex-around");
 subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
-console.dir(subMenuEl);
 
 // Part 2-4
-console.log("Part 2-4-a: set up topMenuLinks");
 let topMenuLinks = document.querySelectorAll("#top-menu a");
-console.dir(topMenuLinks);
 
 // apPARently, I shouldn't use "anonymous functions" as "callback functions"
 // because the handlers can't be removed "safely" or maybe "at all"
@@ -106,43 +102,86 @@ console.dir(topMenuLinks);
 // Actually, that article was from 2013. The current documentation indicates it's NBD.
 // const handlerTopMenuClick = (e) => {
 
-console.log("Part 2-4-b: set up event listener and event handler for top nav click");
 topMenuEl.addEventListener("click", handlerTopMenuClick);
-
-console.log("Part 2-4-c: manage menu items' \"active\" states");
-// within handler
-console.log("Part 2-5: submenu interaction");
-// within handler
-
-
-
 
 console.groupEnd();
 
+
 function handlerTopMenuClick(e) {
-  console.group("Top Nav click handling");
+  // when we sense a click in the upper nav, do stuff.
+  // console.group("Handling top-level nav clicks");
+
+  let newNodes = [];
+  let subMenu;
+  let subMenuRaw = [];    // the submenu we want, as it appears in `menuLinks`
+
+  // prevent default behavior: in this case... don't go reloading the page?
   e.preventDefault();
 
-  if (e.target.tagName != "A") {
-    return;
-  }
+  if (e.target.tagName != "A") return;  // ignore clicks in the nav bar's dead space
 
-  // debugger;
+  // hide the secondary nav bar straight away
+  //   doing this for everything simplifies the logic below, but it's lazy.
+  //   if this is too slow, it may cause ugly flicker/redraw if we actually
+  //   want to secondary nav bar to stay. test it out.
   subMenuEl.style.top = 0;
+  // walk through all the top menu items, not just the one clicked on; a click
+  // on one item will affect the others.
   for (let node of topMenuLinks) {
-    console.count("DEBUG > handlerTopMenuClick > inner loop");
+    // desired main nav behavior: at most one item "active" at a time
+    // clicking on the active item deactivates it, reverting to initial state.
     if (node == e.target && !node.classList.contains("active")) {
-      console.debug(`activating menu bar item ${node.textContent}`)
       node.classList.add("active");
-      if (node.attributes.href.value
-        == '#') {
-        console.debug(`activating submenu`)
+      if (node.attributes.href.value == '#') {  // menu pointing at '#' have submenus
+        // search the menu data structure for the right submenu
+        for (let menuItem of menuLinks) {
+          if (menuItem.text == node.textContent) {
+            
+
+          }
+        }
+        // console.debug(`populating submenu`)
+        // if replace doesn't work: Array.from(subMenuEl.children).forEach((link) => link.remove());
+        subMenuEl.replaceChildren(
+          buildSubmenu(node.attributes.subLinks)
+        );
         subMenuEl.style.top = '100%';
       }
     } else {
-      console.debug(`deactivating menu bar item ${node.textContent}`)
+      // console.debug(`deactivating menu bar item ${node.textContent}`)
       node.classList.remove("active");
     };
   };
+
+  // console.groupEnd();
 };
+
+function buildSubmenu(subLinks) {
+  // receives an array of dict-like objects
+  // representing the sublinks for the menu being exposed
+  // adds them to...TBD?
+  // rebuilds each time the submenu changes
+  const newSubmenu = [];
+  for (let link of subLinks) {
+    let newLink = document.createElement('a');
+    newLink.setAttribute("href", link.href);
+    newLink.textContent = link.text;
+    newSubmenu.appendChild(newLink);
+  };
+  subMenuEl.replaceChildren(newSubmenu);
+  console.dir(subMenuEl);
+}
+
+/* 
+The submenu needs to be dynamic based on the clicked link. To facilitate that.
+function buildSubmenu(subLinks)
+* Clear the current contents of subMenuEl.
+* Iterate over subLinks; for each "link" object:
+  * Create an <a> element.
+  * Add an href attribute, set to link.href
+  * set the element's content to link.text
+  * Append the new element to the subMenuEl.
+Add it to the event listener. within the same logic that shows the submenu, remembering to pass the array of sub-links as an argument.
+ */
+
 
