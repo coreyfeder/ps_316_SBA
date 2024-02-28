4ff4bdbefa79049bcae64e7762f3c3b305a0f086
Corey Feder <corey.feder@gmail.com>
2024-02-28 03:52:39 -0500

    refactoring to be less assignment-section-based

--- README.md
+++ README.md
@@ -1,24 +1,37 @@
 # ALAB 316: DOM Manipulation
 
-## ALAB 316.1.1: DOM Manipulation (Part One)
-
-https://www.canva.com/design/DAFriiiv6ao/a0I0JHC1iA_ZzgK2qv6itg/view
-
-`import` statements don't seem to work locally.
-Developing in codesandbox. Codesandboxdevbox.
-https://3zwy94-1234.csb.app/
+  * [ALAB 316.1.1: DOM Manipulation (Part One)](https://www.canva.com/design/DAFriiiv6ao/a0I0JHC1iA_ZzgK2qv6itg/view)
+  * [R-ALAB 316.3.1: DOM Manipulation (Part Two)](https://www.canva.com/design/DAFrigQgiRw/WFebhXc9I-hpJappzXGPNQ/view)
 
 
-### Instructions
+## Summary: Instructions & Objectives
 
-_Do not_ modify any of the contents of the `index.html` or `styles.css` files. Your goal in this lab is to __demonstrate DOM manipulation through JavaScript__. As such, directly modifying the HTML or CSS files is counterproductive.
-
-Take five minutes to familiarize yourself with [CSS Custom Properties](https://www.canva.com/link?target=https%3A%2F%2Fdeveloper.mozilla.org%2Fen-US%2Fdocs%2FWeb%2FCSS%2FUsing_CSS_custom_properties&design=DAFriiiv6ao&accessRole=viewer&linkSource=document) (variables) - they are an amazing addition to CSS. If you are familiar with using variables with SASS/LESS pre-processors, CSS Custom Properties are similar but far more powerful because they are dynamic (their values can be changed during runtime) - and they are built into the CSS language!
+Create a two-tiered navigation bar and give it some event-driven interactive elements..._without_ making any changes to the HTML. Instead, use JavaScript to manipulate the DOM.
 
 
-## Questions
+## Implementation Notes
 
-1. How do I make the `import` statement work?
-1. How do I make the `import` statement work reliably?
-1. Is importing the css to the js file the only way to access the info?
-    1. Why can't the js see the css if the css has been linked as normal?
+### `import "./styles.css";`
+
+Well, THAT line didn't work locally. Best I can figure is this is some misplaced remnant of Next.js or SASS or something. Probably used by accident, or maybe someone thought it necessary for the exercise, or maybe it's a trap by CodeSandbox to keep people on their site.
+
+NBD if CodeSandbox were working more reliably; but I'd argue it shouldn't be in a template or boilerplate of an exercise. We can still be put through the DOM manipulation paces even if the page is properly linked to a stylesheet.
+
+Only worth noting because this has been an *enormous* headache trying to set up to work locally. I've spent *hours* trying to work around this "correctly," without feeling like I'm undermining the exercise goals. It's insane. I kept thinking we'd hash it out at office hours, but stuff came up and they were cancelled. (Hey, real life happens.)
+
+Anyway, for the record: CodeSandbox was causing much _bigger_ headaches. I can at least try to work around weird import routes misbehaving, but malfunctioning Service Worker registration and straight-up nondeterministic rendering are just too much.
+
+So...maybe _technically_ I'm staying in bounds because I'm not changing the HTML, just the DOM? But I'm directly undoing something probably done deliberately. It feels slimy, but it's all just pseudocode if I can't actually load the page.
+
+### `buildSubmenu`
+
+I removed this helper. We have already created and inserted nodes. For the submenu, I create the nodes beforehand, and let them hang about as hidden children of the top-level nav nodes. Then, when it's time to activate or deactivate them, the nodes move onto or off of the secondary nav.
+
+I don't know the overhead of moving nodes versus changing their makeup, but I expect moving structural DOM around is much _worse_, so I wouldn't do this in a live environment. I just hate searching for things in lists. Traverse, compare, break when you find it. Then build another of a thing we already have. Moving nodes about is more interesting and challenging. And the assignment does say, "There are many ways to arrive at the same solution in development...If it works, good job!" So hopefully it keeps working.
+
+### `console.log`
+
+This is the worst `print` statement I've ever seen. It feels completely random whether it will produce a useful line of text, a mixture of useful and obtude text, an obscenely-long list of default object properties burying any useful information, or—most infuriatingly—just the word "object". It may as well say "bits."
+
+But it turns out there are other options! Different log levels, log message grouping, built-in counters, and... offs, _why didn't anyone tell us about `debugger`?_ :cry: Isn't fussing with the live data the whole _point_ of using a console??
+
--- src/index.js
+++ src/index.js
@@ -1,33 +1,15 @@
 // import "./styles.css";
-/* 
-Well, THAT line doesn't work locally.
-Best I can figure is this is some misplaced remnant of Next.js or SASS or something. Probably used by accident, or maybe someone thought it necessary for the exercise, or maybe it's a trap by CodeSandbox to keep people on their site.
 
-Maybe NBD if you're sure people will stay and work in CodeSandbox, if CodeSandbox were working more reliably; but I'd argue it shouldn't be in a template or boilerplate of an exercise. We can still be put through the DOM manipulation paces even if the page is properly linked to a stylesheet.
-
-Only worth noting because this has been an *enormous* headache trying to set up to work locally. I've spent *hours* trying to work around this "correctly," without feeling like I'm undermining the exercise goals. It's insane. I kept thinking we'd hash it out at office hours, but stuff came up and they were cancelled. Hey, real life happens.
-
-Anyway, for the record: CodeSandbox was causing much _bigger_ headaches. I can sort of deal with weird import routes, but malfunctioning Service Worker registration and straight-up nondeterministic rendering are just too much.
-
-So..._technically_ staying in bounds because I'm not changing the HTML, just the DOM. It feels slimy, but I can't get any work done if I can't actually load the page.
- */
+// ...welp, that didn't work. And I'm failing to make it work.
+// So, I'm sticking it back where it wants to be.
 let cssLink = document.createElement('link')
 cssLink.setAttribute('rel', 'stylesheet');
 cssLink.setAttribute('href', 'src/styles.css');
 document.getElementsByTagName('head')[0].appendChild(cssLink);
 
-/*
-Also, we have already created and destroyed nodes. For the submenu, I'm creating the nodes beforehand, and having them hang about as hidden children of the top-level nav nodes. Then, when it's time to activate or deactivate them, I'll move the nodes.
-
-I don't know the overhead of moving nodes versus changing their makeup, but I expect moving structural DOM around is much worse, so I wouldn't do this in a live environment. I just hate searching for things in lists. Traverse, compare, break when you find it. Then build another of a thing we already have. Moving nodes about is much more interesting and challenging.
- */
-
-/* 
-Holy crap. Turns out, there are a bunch of output tools besides `console.log()`. Logging at different levels, grouping, built-in counters, and ... OFFS WHY DIDN'T ANYONE TELL US ABOUT `debugger`??
- */
 
 // Menu data structure
-//   (Let's consider this the source data. Don't rearrange or rename anything.)
+//   (Treat this the source data. Don't rearrange or rename anything.)
 const menuLinks = [
   { text: "about", href: "/about" },
   {
@@ -74,15 +56,14 @@ topMenuEl.classList.add("flex-around");
 
 // Part 1-3
 for (let link of menuLinks) {
-  // top-level nav links
+  // create the top-level nav links
   let newLink = document.createElement("a");
   newLink.setAttribute("href", link.href);
   newLink.textContent = link.text;
   topMenuEl.appendChild(newLink);
-  // sub-menu nav links
+  // create the sub-menu nav links and
+  // stash them under/inside their parents
   if (link.subLinks) {
-    // add as anchors and set to hidden
-    // if it causes troubles, add them under a <details> node?
     for (let subLink of link["subLinks"]) {
       let newSubLink = document.createElement("a");
       newSubLink.setAttribute("href", subLink.href);
@@ -92,13 +73,14 @@ for (let link of menuLinks) {
   };
 };
 
-// but we don't want them visible until they're in the submenu.
+// but we don't want the subMenu nodes visible while they're not in the subMenu.
 let submenuStorage = document.createElement("style");
 submenuStorage.textContent = '#top-menu a a { display: none; }';
 document.querySelector("head").appendChild(submenuStorage);
 
+
 //Part 2-3
-let subMenuEl = document.getElementById("sub-menu");
+const subMenuEl = document.getElementById("sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
 subMenuEl.classList.add("flex-around");
@@ -108,28 +90,48 @@ subMenuEl.style.top = "0";
 // Part 2-4
 let topMenuLinks = document.querySelectorAll("#top-menu a");
 
-// apPARently, I shouldn't use "anonymous functions" as "callback functions"
-// because the handlers can't be removed "safely" or maybe "at all"
-// so they will "create memory leaks"
-// and the apps will "slow to a crawl" or "crash"
-// especially with "long-running" web apps
-// (and in this age of "tab proliferation", "every" web app should "expect" to be "long-running")
-// and the "users" will be "angry"
-// and then I'll be "unemployed" "again"
-// and be forced to "learn" to "use" "quotation marks"
-// "pro"per"ly"
-// ...
-// Actually, that article was from 2013. The current documentation indicates it's NBD.
-// const handlerTopMenuClick = (e) => {
-
 topMenuEl.addEventListener("click", handlerTopMenuClick);
 subMenuEl.addEventListener("click", handlerSubMenuClick);
+
+
+function getActive() {
+  // would querySelector(".active") be better? or getElementsByClassName("active")?
+  return document.querySelector("#top-menu a.active");
+}
+
+function isActive(menuEl) {
+  return menuEl.classList.contains("active");
+}
+
+function activate(menuEl) {
+  console.assert(!isActive(menuEl), "Cannot activate active menu item. " + menuEl);
+  menuEl.classList.remove('active')
+}
+
+function deactivate(menuEl) {
+  console.assert(isActive(menuEl), "Cannot deactivate inactive menu item. " + menuEl);
+  menuEl.classList.remove('active')
+}  
+
+function isClosed(subMenuEl) {
+  return subMenuEl.style.top == "0px";
+}
+
+function closeSubMenu(subMenuEl) {
+  subMenuEl.style.top == 0;
+}
+
+function hasSubMenu(navNode) {
+  // wait..  navNode.getAttribute('href') != navNode.href  ...are you kidding?
+  return navNode.getAttribute('href') == '#';
+}  
 
 
 function moveChildLinks(childrenFrom, childrenTo) {
   // Returns true if any child elements were moved.
   let result = false;
-  console.assert(childrenTo.children.length == 0, "This primary navigation item already has its secondary links.");
+  // don't use .children.length == 0 to check for children when I actually mean to check for children who are Elements
+  console.assert(childrenTo.firstElementChild === null, "This primary navigation item already has its secondary links.");
   while (childrenFrom.firstElementChild != null) {
     childrenTo.appendChild(childrenFrom.firstElementChild);
     result = true;
@@ -137,16 +139,10 @@ function moveChildLinks(childrenFrom, childrenTo) {
   return result;
 }
 
-
-function hasSubMenu(navNode) {
-  // navNode.getAttribute('href') != navNode.href  ...are you f'n kidding me?
-  return navNode.getAttribute('href') == '#';
-}
-
-
 function closeMenu() {
-  if (subMenuEl.style.top != "0px") {
-    subMenuEl.style.top = 0;
+  // close subMenu and stow links
+  if (!isClosed(subMenuEl)) {
+    closeSubMenu(subMenuEl);
   };
   let activeEl = document.querySelector("#top-menu a.active");
   if (activeEl) {
@@ -169,8 +165,8 @@ function handlerTopMenuClick(e) {
   for (let node of topMenuLinks) {
     // desired main nav behavior: at most one item "active" at a time
     // clicking on the active item deactivates it, reverting to initial state.
-    if (node == e.target && !node.classList.contains("active")) {
-      node.classList.add("active");
+    if (node == e.target && !isActive(node)) {
+      activate(node);
       if (hasSubMenu(node)) {
         moveChildLinks(node, subMenuEl);
         subMenuEl.style.top = '100%';
