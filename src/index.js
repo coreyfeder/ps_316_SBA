2b80e38e28d02710c8da329d2e96e4c3cd0f28bb
Corey Feder <corey.feder@gmail.com>
2024-02-28 05:52:28 -0500

    i just HAD to fuss

--- src/index.js
+++ src/index.js
@@ -40,59 +40,61 @@ const menuLinks = [
   },
 ];
 
-// Part 1-1
+
 const mainEl = document.querySelector("main");
-mainEl.style.backgroundColor = "var(--main-bg)";
-let newNode = document.createElement("h1");
-newNode.textContent = "DOM Manipulation";
-mainEl.appendChild(newNode);
-mainEl.classList.add("flex-ctr");
+const topMenuEl = document.getElementById("top-menu");
+const subMenuEl = document.getElementById("sub-menu");
+const topMenuLinks = document.querySelectorAll("#top-menu a");
 
-//Part 1-2
-let topMenuEl = document.getElementById("top-menu");
-topMenuEl.style.height = "100%";
-topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
-topMenuEl.classList.add("flex-around");
 
-// Part 1-3
-for (let link of menuLinks) {
-  // create the top-level nav links
-  let newLink = document.createElement("a");
-  newLink.setAttribute("href", link.href);
-  newLink.textContent = link.text;
-  topMenuEl.appendChild(newLink);
-  // create the sub-menu nav links and
-  // stash them under/inside their parents
-  if (link.subLinks) {
-    for (let subLink of link["subLinks"]) {
-      let newSubLink = document.createElement("a");
-      newSubLink.setAttribute("href", subLink.href);
-      newSubLink.textContent = subLink.text;
-      newLink.appendChild(newSubLink);
+// init
+( () => {
+  let newNode;
+  
+  mainEl.style.backgroundColor = "var(--main-bg)";
+  newNode = document.createElement("h1");
+  newNode.textContent = "DOM Manipulation";
+  mainEl.appendChild(newNode);
+  mainEl.classList.add("flex-ctr");
+
+  topMenuEl.style.height = "100%";
+  topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+  topMenuEl.classList.add("flex-around");
+
+  subMenuEl.style.height = "100%";
+  subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+  subMenuEl.classList.add("flex-around");
+  subMenuEl.style.position = "absolute";
+  subMenuEl.style.top = "0";
+  
+  for (let link of menuLinks) {
+    // create the top-level nav links
+    let newLink = document.createElement("a");
+    newLink.setAttribute("href", link.href);
+    newLink.textContent = link.text;
+    topMenuEl.appendChild(newLink);
+    // create the sub-menu nav links as children
+    if (link.subLinks) {
+      for (let subLink of link["subLinks"]) {
+        let newSubLink = document.createElement("a");
+        newSubLink.setAttribute("href", subLink.href);
+        newSubLink.textContent = subLink.text;
+        newLink.appendChild(newSubLink);
+      };
     };
   };
-};
+  // but keep the subMenu nodes hidden while they're not in the subMenu.
+  let submenuStorage = document.createElement("style");
+  submenuStorage.textContent = '#top-menu a a { display: none; }';
+  document.querySelector("head").appendChild(submenuStorage);
 
-// but we don't want the subMenu nodes visible while they're not in the subMenu.
-let submenuStorage = document.createElement("style");
-submenuStorage.textContent = '#top-menu a a { display: none; }';
-document.querySelector("head").appendChild(submenuStorage);
+  topMenuEl.addEventListener("click", handlerTopMenuClick);
+  subMenuEl.addEventListener("click", handlerSubMenuClick);
+
+})();  
 
 
-//Part 2-3
-const subMenuEl = document.getElementById("sub-menu");
-subMenuEl.style.height = "100%";
-subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
-subMenuEl.classList.add("flex-around");
-subMenuEl.style.position = "absolute";
-subMenuEl.style.top = "0";
-
-// Part 2-4
-let topMenuLinks = document.querySelectorAll("#top-menu a");
-
-topMenuEl.addEventListener("click", handlerTopMenuClick);
-subMenuEl.addEventListener("click", handlerSubMenuClick);
-
+// don't pass gloabls to the helper functions
 
 function getActive() {
   // would querySelector(".active") be better? or getElementsByClassName("active")?
@@ -105,27 +107,35 @@ function isActive(menuEl) {
 
 function activate(menuEl) {
   console.assert(!isActive(menuEl), "Cannot activate active menu item. " + menuEl);
-  menuEl.classList.remove('active')
+  menuEl.classList.add('active')
 }
 
 function deactivate(menuEl) {
-  console.assert(isActive(menuEl), "Cannot deactivate inactive menu item. " + menuEl);
-  menuEl.classList.remove('active')
+  // console.assert(isActive(menuEl), "Cannot deactivate inactive menu item. " + menuEl);
+  if (menuEl) {
+    menuEl.classList.remove('active')
+  } else {
+    return false;
+  }
 }  
 
-function isClosed(subMenuEl) {
+function isSubmenuCollapsed() {
   return subMenuEl.style.top == "0px";
 }
 
-function closeSubMenu(subMenuEl) {
-  subMenuEl.style.top == 0;
+function collapseSubmenu() {
+  subMenuEl.style.top = "0";
 }
 
-function hasSubMenu(navNode) {
+function expandSubmenu() {
+  subMenuEl.style.top = "100%";
+}
+
+function hasSubmenu(navNode) {
   // wait..  navNode.getAttribute('href') != navNode.href  ...are you kidding?
+  if (!navNode) return false;
   return navNode.getAttribute('href') == '#';
 }  
-
 
 function moveChildLinks(childrenFrom, childrenTo) {
   // Returns true if any child elements were moved.
@@ -139,18 +149,38 @@ function moveChildLinks(childrenFrom, childrenTo) {
   return result;
 }
 
-function closeMenu() {
-  // close subMenu and stow links
-  if (!isClosed(subMenuEl)) {
-    closeSubMenu(subMenuEl);
-  };
-  let activeEl = document.querySelector("#top-menu a.active");
-  if (activeEl) {
-    moveChildLinks(subMenuEl, activeEl);
-    activeEl.classList.remove('active');
+function stowSubmenuLinks(navNode) {
+  console.assert(hasSubmenu(navNode), "Stowing submenu nav links in a nav node without any subnav!");
+  console.assert(isActive(navNode), "Stowing submenu nav links in a non-active nav parent node.");
+  moveChildLinks(subMenuEl, navNode);
+}
+
+function presentSubmenuLinks(navNode) {
+  // console.assert(hasSubmenu(navNode), "This nav node does not have any subnmenu nav to present!");
+  // console.assert(isActive(navNode), "Presenting submenu nav links of a non-active nav parent node.");
+  if (hasSubmenu(navNode) && isActive(navNode)) {
+    moveChildLinks(navNode, subMenuEl);
+    expandSubmenu();
   }
 }
 
+function shutdownNav() {
+  // stow links as needed then collapse subMenu
+  if (!isSubmenuCollapsed()) {
+    stowSubmenuLinks(getActive());
+    collapseSubmenu();
+  };
+  deactivate(getActive());
+};
+
+function startupNav(navNode) {
+  if (navNode) {
+    activate(navNode);
+    if (hasSubmenu(navNode)) {
+      presentSubmenuLinks(navNode);
+    }
+  }
+}
 
 function handlerTopMenuClick(e) {
   // handle clicks in the primary nav bar
@@ -158,25 +188,12 @@ function handlerTopMenuClick(e) {
   if (e.target.tagName != "A") return;  // ignore clicks in the nav bar's dead space
   console.log(e.target.innerText);
 
-  closeMenu();  // hide and empty the secondary nav bar straight away
+  debugger;
+  let nextActiveNode = isActive(e.target) ? null : e.target;
+  shutdownNav();
+  startupNav(nextActiveNode);
 
-  // walk through all the top menu items, not just the one clicked on; a click
-  // on one item will affect the others.
-  for (let node of topMenuLinks) {
-    // desired main nav behavior: at most one item "active" at a time
-    // clicking on the active item deactivates it, reverting to initial state.
-    if (node == e.target && !isActive(node)) {
-      activate(node);
-      if (hasSubMenu(node)) {
-        moveChildLinks(node, subMenuEl);
-        subMenuEl.style.top = '100%';
-      }
-    } else {
-      node.classList.remove("active");
-    };
-  };
-
-  if (!hasSubMenu(e.target)) {
+  if (!hasSubmenu(nextActiveNode)) {
     document.querySelector('main > h1').textContent = e.target.innerText;
   }
 };
