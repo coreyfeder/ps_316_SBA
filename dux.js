849695bdd8ced67156395efcfa7c3a759733971e
Corey Feder <corey.feder@gmail.com>
2024-02-28 23:23:15 -0500

    begin Pokédux

---
+++ dux.js
@@ -0,0 +1,8 @@
+let beaks_to_try_on = [
+    'img/a perfectly normal beak.png',
+];
+
+let beak = beaks_to_try_on[0];
+
+document.querySelector('div.beak > img').setAttribute('src', trythisone);
+document.querySelector('img.beak').setAttribute('src', trythisone);
